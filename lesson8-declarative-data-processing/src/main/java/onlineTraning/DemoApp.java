82421cd526092ec719d966e86ade8c7a45092420
victoria <victoria.mirskaya@mustpay.tech>
2022-05-28 07:53:11 +0100

    Lesson 8: online part

---
+++ lesson8-declarative-data-processing/src/main/java/onlineTraning/DemoApp.java
@@ -0,0 +1,27 @@
+package onlineTraning;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class DemoApp {
+
+    /**
+     * Using one Stream API statement find a sum of all prime numbers from 501st to 520th inclusively.
+     * A prime number is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+     * Please note that 501 and 520 is a position in the list of prime numbers, not a value:exclamation:️ E.g. take a look at the first five prime numbers:
+     * 2
+     * 3
+     * 5
+     * 7
+     * 11
+     * …
+     */
+
+//    public static void main(String[] args) {
+//        Integer sum = IntStream.range(1, 520)
+//                .filter(((a,b) -> (b!=a && b!=1) && (a/b) != 0)
+//                .sum();
+//    }
+
+
+}
---
+++ lesson8-declarative-data-processing/src/main/java/onlineTraning/warmUp.java
@@ -0,0 +1,29 @@
+package onlineTraning;
+
+import com.vikmir.data.Accounts;
+import com.vikmir.model.Account;
+
+import java.util.Comparator;
+
+public class warmUp {
+
+    /**
+     *     Implement  a custom method createComparatorComparing
+     *     Declare a generic method that accepts an extraction function and returns a comparator
+     *     Specify method type parameters to make sure that extraction function accepts a target type as input param and returns a comparable type
+     *     Inside the method implement a comparing logic based on values that were computed using extraction function
+     *     Return comparator instance
+     */
+
+//    public static void main(String[] args) {
+//        var accountWithMaxBalance = Accounts.generateAccountList(10)
+//                .stream()
+//                .max(createComparatorComparing(Account::getBalance))
+//                .orElseThrow();
+//    }
+//
+//    private static <T extends Comparable<T>> Comparator<T> createComparatorComparing(T getBalance) {
+//        return null;
+//    }
+
+}
